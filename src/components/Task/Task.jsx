59fe6e50628d9913ee9a2ec392f7d6fb6ebff329
Aleksey Shingarev <alekseytim@mail.ru>
2023-04-02 13:54:57 +0300

    feat: tasks works

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,142 +1,3 @@
-// import React, { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import { deleteTask, editTask, markTaskDone, addSubtask } from "../../slices/projectSlice";
-// import classNames from 'classnames';
-// import { v4 as uuidv4 } from 'uuid';
-// import styles from './task.module.scss';
-
-// const Task = ({ projectId, id, title, subtasks, done }) => {
-//   const dispatch = useDispatch();
-//   const [newTitle, setNewTitle] = useState(title || "");
-//   const [editMode, setEditMode] = useState(false);
-//   const [showAddSubtaskModal, setShowAddSubtaskModal] = useState(false);
-//   const [newSubtaskTitle, setNewSubtaskTitle] = useState('');
-
-//   const handleDelete = () => {
-//     dispatch(deleteTask({ projectId: projectId, taskId: id }));
-//   };
-
-//   const handleToggle = () => {
-//     dispatch(markTaskDone({ projectId, taskId: id, done: !done }));
-//   };
-
-//   const handleEdit = () => {
-//     if (projectId && id && newTitle) {
-//       dispatch(editTask({ projectId, taskId: id, newTitle: newTitle }));
-//       setEditMode(false);
-//     }
-//   };
-
-//   const handleCancelEdit = () => {
-//     setNewTitle(title);
-//     setEditMode(false);
-//   };
-
-//   const handleKeyDown = (event) => {
-//     if (event.key === "Enter") {
-//       handleEdit();
-//     } else if (event.key === "Escape") {
-//       handleCancelEdit();
-//     }
-//   };
-
-//   const handleAddSubtask = (e) => {
-//     e.preventDefault();
-//     const newSubtask = {
-//       id: uuidv4(),
-//       title: newSubtaskTitle,
-//       subtasks: []
-//     };
-//     dispatch(addSubtask({ projectId, taskId: id, subtask: newSubtask }));
-//     setShowAddSubtaskModal(false);
-//     setNewSubtaskTitle('');
-//   };
-
-//   return (
-//     <div className={styles.task_container}>
-//       <div className={styles.task}>
-//         {editMode ? (
-//           <input
-//             type="text"
-//             className={styles.task_edit_input}
-//             value={newTitle}
-//             onChange={(event) => setNewTitle(event.target.value)}
-//             onKeyDown={handleKeyDown}
-//           />
-//         ) : (
-//           <p className={classNames(styles.task_title, { [styles.done]: done })}>
-//             {newTitle}
-//           </p>
-//         )}
-//         <div className={styles.task_buttons}>
-//           {editMode ? (
-//             <>
-//               <button className={styles.task_button} onClick={handleEdit}>
-//                 Save
-//               </button>
-//               <button
-//                 className={styles.task_button}
-//                 onClick={handleCancelEdit}
-//               >
-//                 Cancel
-//               </button>
-//             </>
-//           ) : (
-//             <>
-//               <button className={styles.task_button} onClick={handleToggle}>
-//                 {done ? "Not done" : "Done"}
-//               </button>
-//               <button
-//                 className={classNames(
-//                   styles.delete_task_button,
-//                   styles.task_button
-//                 )}
-//                 onClick={handleDelete}
-//               >
-//                 Delete
-//               </button>
-//               <button
-//                 className={styles.task_button}
-//                 onClick={() => setEditMode(true)}
-//               >
-//                 Edit
-//               </button>
-//               <button className={styles.task_button} onClick={() => setShowAddSubtaskModal(true)}>
-//                 Add subtask
-//               </button>
-//               {showAddSubtaskModal && (
-//                 <div className={styles.modal}>
-//                   <form onSubmit={handleAddSubtask}>
-//                     <input type="text" value={newSubtaskTitle} onChange={e => setNewSubtaskTitle(e.target.value)} />
-//                     <button type="submit">Add</button>
-//                     <button type="button" onClick={() => setShowAddSubtaskModal(false)}>Cancel</button>
-//                   </form>
-//                 </div>
-//               )}
-//             </>
-//           )}
-//         </div>
-//       </div>
-//       <div className={styles.task_container}>
-//         {subtasks && subtasks.map((subtask) => (
-//           <div className={styles.subtask} key={subtask.id}>
-//             <Task projectId={projectId} key={uuidv4()} subtasks={subtask.subtasks} id={subtask.id} title={subtask.name} done={false} />
-//           </div>
-//         ))}
-//         {subtasks && subtasks.map((subtask) => (
-//           <div className={styles.subtask} key={subtask.id}>
-//             {subtask.subtasks && subtask.subtasks.length > 0 && (
-//               <Task projectId={projectId} key={uuidv4()} subtasks={subtask.subtasks} id={subtask.id} title={subtask.name} done={false} />
-//             )}
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Task;
-
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { deleteTask, editTask, markTaskDone, addSubtask } from "../../slices/projectSlice";
@@ -152,20 +13,21 @@ const Task = ({ projectId, id, title, subtasks, done, parentId }) => {
   const [newSubtaskTitle, setNewSubtaskTitle] = useState('');
 
   const handleDelete = () => {
-    dispatch(deleteTask({ projectId: projectId, taskId: id }));
+    if (parentId) {
+      dispatch(deleteTask({ projectId, taskId: id, parentId }));
+    } else {
+      dispatch(deleteTask({ projectId, taskId: id }));
+    }
   };
-
-  // const handleDelete = () => {
-  //   if (parentId) {
-  //     dispatch(deleteTask({ projectId, taskId: parentId, subtaskId: subtaskId }));
-  //   } else {
-  //     dispatch(deleteTask({ projectId, taskId: id }));
-  //   }
-  // };
 
   const handleToggle = () => {
-    dispatch(markTaskDone({ projectId, taskId: id, done: !done }));
+    if (parentId) {
+      dispatch(markTaskDone({ projectId, taskId: id, parentId, done: !done }));
+    } else {
+      dispatch(markTaskDone({ projectId, taskId: id, done: !done }));
+    }
   };
+
 
   const handleEdit = () => {
     if (projectId && id && newTitle) {
@@ -209,7 +71,13 @@ const Task = ({ projectId, id, title, subtasks, done, parentId }) => {
     if (subtasks.length > 0) {
       return subtasks.map((subtask) => (
         <div className={styles.subtask} key={subtask.id}>
-          <Task projectId={projectId} subtasks={subtask.subtasks} id={subtask.id} title={subtask.name} done={subtask.done} />
+          <Task
+            projectId={projectId}
+            subtasks={subtask.subtasks}
+            id={subtask.id}
+            title={subtask.name}
+            done={subtask.done}
+          />
         </div>
       ));
     }
--- src/slices/projectSlice.js
+++ src/slices/projectSlice.js
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { saveState } from '../utils/localStorage';
 import { findParentSubtask } from '../utils/findSubtask';
+import { findTaskById, setTaskDone } from '../utils/findTaskById';
 import { v4 as uuidv4 } from 'uuid';
 
 const initialState = {
@@ -46,51 +47,104 @@ export const projectSlice = createSlice({
       const project = state.projects.find(project => project.id === projectId);
 
       if (project) {
-        const task = project.tasks.find(task => task.id === taskId);
-        if (task) {
-          task.name = newTitle;
-          saveState(state);
+        const editInSubtasks = (subtasks) => {
+          for (let i = 0; i < subtasks.length; i++) {
+            if (subtasks[i].id === taskId) {
+              subtasks[i].name = newTitle;
+              return true;
+            } else if (subtasks[i].subtasks && editInSubtasks(subtasks[i].subtasks)) {
+              return true;
+            }
+          }
+          return false;
+        };
+
+        // Check if the task to edit is a subtask
+        let editedInSubtasks = false;
+        for (let i = 0; i < project.tasks.length; i++) {
+          const task = project.tasks[i];
+          if (editInSubtasks(task.subtasks)) {
+            editedInSubtasks = true;
+            break;
+          }
+        }
+
+        // If it's not a subtask, edit it in the parent task's tasks array
+        if (!editedInSubtasks) {
+          const taskToEdit = project.tasks.find(task => task.id === taskId);
+          if (taskToEdit) {
+            taskToEdit.name = newTitle;
+            saveState(state);
+          }
         }
       }
     },
     deleteTask: (state, action) => {
-      const project = state.projects.find(project => project.id === action.payload.projectId);
-      if (project) {
-        project.tasks = project.tasks.filter(task => task.id !== action.payload.taskId);
-      }
-      // const { projectId, taskId, subtaskId } = action.payload;
+      const { projectId, taskId } = action.payload;
+      const project = state.projects.find(project => project.id === projectId);
 
-      // console.log(projectId);
-      // console.log(taskId);
-      // console.log(subtaskId);
-      // const project = state.projects.find(project => project.id === projectId);
-      // if (project) {
-      //   const task = project.tasks.find(task => task.id === taskId);
-      //   if (task) {
-      //     const subtaskIndex = task.subtasks.findIndex(subtask => subtask.id === subtaskId);
-      //     if (subtaskIndex !== -1) {
-      //       task.subtasks.splice(subtaskIndex, 1);
-      //     }
-      //   }
-      // }
+      if (project) {
+        const deleteFromSubtasks = (subtasks) => {
+          for (let i = 0; i < subtasks.length; i++) {
+            if (subtasks[i].id === taskId) {
+              subtasks.splice(i, 1);
+              return true;
+            } else if (subtasks[i].subtasks && deleteFromSubtasks(subtasks[i].subtasks)) {
+              return true;
+            }
+          }
+          return false;
+        };
+
+        // Check if the task to delete is a subtask
+        let deletedFromSubtasks = false;
+        for (let i = 0; i < project.tasks.length; i++) {
+          const task = project.tasks[i];
+          if (deleteFromSubtasks(task.subtasks)) {
+            deletedFromSubtasks = true;
+            break;
+          }
+        }
+
+        // If it's not a subtask, delete it from the parent task's tasks array
+        if (!deletedFromSubtasks) {
+          project.tasks = project.tasks.filter(task => task.id !== taskId);
+        }
+      }
     },
     markTaskDone: (state, action) => {
       const { projectId, taskId, done } = action.payload;
       const project = state.projects.find(project => project.id === projectId);
+
       if (project) {
-        const task = project.tasks.find(task => task.id === taskId);
-        if (task) {
-          task.done = done;
+        const markInSubtasks = (subtasks) => {
+          for (let i = 0; i < subtasks.length; i++) {
+            if (subtasks[i].id === taskId) {
+              subtasks[i].done = done;
+              return true;
+            } else if (subtasks[i].subtasks && markInSubtasks(subtasks[i].subtasks)) {
+              return true;
+            }
+          }
+          return false;
+        };
+
+        // Check if the task to mark as done is a subtask
+        let markedInSubtasks = false;
+        for (let i = 0; i < project.tasks.length; i++) {
+          const task = project.tasks[i];
+          if (markInSubtasks(task.subtasks)) {
+            markedInSubtasks = true;
+            break;
+          }
         }
-      }
-    },
-    markTaskForDeletion: (state, action) => {
-      const { projectId, taskId, markForDeletion } = action.payload;
-      const project = state.projects.find(project => project.id === projectId);
-      if (project) {
-        const task = project.tasks.find(task => task.id === taskId);
-        if (task) {
-          task.markedForDeletion = markForDeletion;
+
+        // If it's not a subtask, mark it as done in the parent task's tasks array
+        if (!markedInSubtasks) {
+          const task = project.tasks.find(task => task.id === taskId);
+          if (task) {
+            task.done = done;
+          }
         }
       }
     },
@@ -144,7 +198,6 @@ export const {
   addTask,
   deleteTask,
   markTaskDone,
-  markTaskForDeletion,
   addSubtask,
   setSearchFilter,
   dragTask,
